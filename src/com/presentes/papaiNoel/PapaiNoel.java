ffdd581cddf05a56165243c4e8e69bd819bdc8b1
Hellimateas <hellimateasbarros@gmail.com>
2022-03-10 20:26:03 -0300

    exercicio 1

--- src/com/presentes/papaiNoel/PapaiNoel.java
+++ src/com/presentes/papaiNoel/PapaiNoel.java
@@ -13,19 +13,18 @@ public class PapaiNoel {
 		List<Criancas> criancas = new ArrayList<>();
 		int entregue = 0;
 		int naoEntregue = 0;
-		
+
 		while (verificador == true) {
 			Criancas crianca = new Criancas();
 			System.out.println("HO-HO-HO Qual é o seu nome minha criança?");
 			crianca.setNome(input.nextLine());
-			
-			
+
 			System.out.println("Você foi uma criança ");
 			System.out.println("1 - BOA");
 			System.out.println("2 - MÁ");
 			int numero = input.nextInt();
 			clearBuffer(input);
-			
+
 			switch (numero) {
 			case 1:
 				crianca.setPersonalidade("BOA");
@@ -37,30 +36,31 @@ public class PapaiNoel {
 			}
 
 			criancas.add(crianca);
-			
+
 			System.out.println("Tem mais algum pequenino? se sim, digite 1. caso não, digite 2 ");
-			if(input.nextInt() == 2) {
+			if (input.nextInt() == 2) {
 				verificador = false;
 			}
 			clearBuffer(input);
-			
-			if (crianca.getPersonalidade().equals("BOA")){
-	             entregue+=1;
-	         } else if (crianca.getPersonalidade().equals("MÁ")) {
-	             naoEntregue+=1;
-	         }
+
+			if (crianca.getPersonalidade().equals("BOA")) {
+				entregue += 1;
+			} else if (crianca.getPersonalidade().equals("MÁ")) {
+				naoEntregue += 1;
+			}
 
 		}
-		 
-		
-		System.out.printf("foram entregues %d presentes \nE %d crianças não receberam por serem levadas!",entregue, naoEntregue );
+
+		System.out.printf("foram entregues %d presentes \nE %d crianças não receberam por serem levadas!", entregue,
+				naoEntregue);
 
 		input.close();
 	}
-	 private static void clearBuffer(Scanner scanner) {
-	        if (scanner.hasNextLine()) {
-	            scanner.nextLine();
-	        }
-	    }
+
+	private static void clearBuffer(Scanner scanner) {
+		if (scanner.hasNextLine()) {
+			scanner.nextLine();
+		}
+	}
 
 }
